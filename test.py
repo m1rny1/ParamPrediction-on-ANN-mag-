9a2b7c4bafe4705232d922c3c2f97bd84a57d3de
Dmytro Myroshnychenko <mir92214@gmail.com>
2023-02-10 22:58:29 +0200

    upd

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 import argparse
-
 import os
-import datetime
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
 import IPython
 import IPython.display
@@ -13,12 +12,15 @@ import seaborn as sns
 import tensorflow as tf
 
 # налаштування кількості епох для тренування
-MAX_EPOCHS = 400
+MAX_EPOCHS = 100
+TRAIN_COUNT = 30
+OUT_STEPS = 2
 
 # функція компіляції та запуску тренування моделі
 def compile_and_fit(model, window, patience=2):
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='loss',   # відслідковування втрат при навчанні
-                                                    patience=patience,    #
+                                                    patience=patience,
+                                                    # restore_best_weights=True,
                                                     mode='min')           # акцент на мінімум втрат
   
   model.compile(loss=tf.losses.MeanSquaredError(),        # втрати обчислюються як середньо-квадратична похибка
@@ -26,9 +28,9 @@ def compile_and_fit(model, window, patience=2):
                 metrics=[tf.metrics.MeanAbsoluteError()]) # метрика базується на середньо-квадратичній похибці
 
   history = model.fit(window.train, epochs=MAX_EPOCHS,  # виконується тренування моделі на основі даних з [window.train]; результат записується у [history]; не більше [MAX_EPOCH] епох
-                      validation_data=window.val,       # валідація виконується усередині [window.val]
-                    #   verbose=0,
-                      callbacks=[early_stopping])       # можливі до застосування callback'и
+                      validation_data=window.val)       # валідація виконується усередині [window.val]
+                      # verbose=1,
+                      # callbacks=[early_stopping])       # можливі до застосування callback'и
   return history
   """Compile and trains selected model in data window
 
@@ -37,70 +39,66 @@ def compile_and_fit(model, window, patience=2):
     at successive epochs, as well as validation loss values
     and validation metrics values (if applicable).
   """
+
 # нвлаштування вікна відображення графіків
 mpl.rcParams['figure.figsize'] = (8, 6)
 mpl.rcParams['axes.grid'] = True
 
-# завантаження датасета (в якості приклада використовується вибірка погодних умов, розподілених за часом)
-parser = argparse.ArgumentParser(description='Filter an xlsx-based data')
-parser.add_argument('-p','--path', type=str, default="input.xlsx",
-                    help='Path to xlsx file with data.')
-parser.add_argument('-s','--sheet', type=int, nargs='+', default=[0],
-                    help='Sheet number. By default using first sheet in file')
-args = parser.parse_args()
+def arg():# завантаження датасета
+  parser = argparse.ArgumentParser(description='Filter an xlsx-based data')
+  parser.add_argument('-p','--path', type=str, default='Al.xlsx',
+                      help='Path to xlsx file with data.')
+  parser.add_argument('-s','--sheet', type=int, nargs='+', default=[1],
+                      help='Sheet number. By default using first sheet in file')
+  args = parser.parse_args()
+path    = 'Al.xlsx' # args.path
+sheet   = 1         # args.sheet[0]
 
-path    = args.path
-sheet   = args.sheet[0]
 
-# зчитування даних із xlsx-файлу
-df = pd.read_excel(path, sheet_name=sheet , header = 0)
+df = pd.read_excel(path, sheet_name=sheet , header = 0, usecols=[2,3])
 print("\nSamples on start::\n", df)
+
 
 # побудова графіку початкових даних
 fig = plt.figure(num="Початкові дані")
 plt.close()
-timer = fig.canvas.new_timer(interval = 60000)
-timer.add_callback(plt.close)
-# plot_cols = ['Подовження, %']
-plot_cols = df.columns[1:] #['MISES 1', 'MISES 2', 'MISES 3', 'MISES 4', 'MISES 5']
+# timer = fig.canvas.new_timer(interval = 60000)
+# timer.add_callback(plt.close)
+plot_cols = df.columns[1:]
 plot_features = df[plot_cols]
 plot_features.index = df[df.columns[0]]
 _ = plot_features.plot(subplots=True)
-timer.start()
+# timer.start()
 plt.show()
 
-# акуратизація даних
+# формування описової статистики
 df.describe().transpose()
-
-# швидке перетворення Фур'є для дискретизації значень температури та розподіл їх у часі
-fft = tf.signal.rfft(df[plot_cols])
-f_per_dataset = np.arange(0, len(fft))
 
 # розділення даних на вибірки (70% тренувальна, 20% для підтвердження, 10% тестова)
 column_indices = {name: i for i, name in enumerate(df.columns)}
-
 n = len(df)                             # загальна кількість елементів
 train_df = df[0:int(n*0.7)]             # 70% тренувальна вибірка
 val_df = df[int(n*0.7):int(n*0.9)]      # 20% для підтвердження результату
-test_df = df[int(n*0.9):]               # решта (10%) тестова
-
+test_df = df[int(n*0.9):]               # решта (10%) тестова 
 num_features = df.shape[1]
+print("Тренувальна вибірка: ", train_df.shape)
+print("Валідаційна вибірка: ", val_df.shape)
+print("Тестова вибірка: ", test_df.shape)
 
-# виконання нормалізації даних
+
+
 train_mean = train_df.mean()    # тренувальне середнє значення
 train_std = train_df.std()      # тренувальне стандартизоване
-
 train_df = (train_df - train_mean) / train_std  # нормалізована тренувальна вибірка
 val_df = (val_df - train_mean) / train_std      # нормалізована вибірка для підтвердження
 test_df = (test_df - train_mean) / train_std    # нормалізована тестова вибірка
-
 # побудова графіку нормалізованих даних
 df_std = (df - train_mean) / train_std
 df_std = df_std.melt(var_name='Column', value_name='Нормалізовано')
 plt.figure(figsize=(12, 6), num="Нормалізовані дані")
 ax = sns.violinplot(x='Column', y='Нормалізовано', data=df_std)
 _ = ax.set_xticklabels(df.keys(), rotation=0)
-timer.start()
+# timer.start()
 plt.show()
 
 # клас, що описує вікно досліджуваних даних
@@ -157,14 +155,16 @@ class WindowGenerator():
     return inputs, labels
   
   # функція для побудови графіку отриманих даних
-  def plot(self, model=None, plot_col=plot_cols[0], max_subplots=3):
+  def plot(self, model=None, plot_col=plot_cols, max_subplots=5):
     inputs, labels = self.example
-    plt.figure(figsize=(8, 8))
-    plot_col_index = self.column_indices[plot_col]
-    max_n = min(max_subplots, len(inputs))
+    plt.figure(figsize=(10, 8))
+    plot_col_index = self.column_indices[plot_col[0]]
+    # plot_col_index = plot_col[0]
+    # max_n = min(max_subplots, len(inputs))
+    max_n = len(plot_col)
     for n in range(max_n):
       plt.subplot(max_n, 1, n+1)
-      plt.ylabel(f'{plot_col} [норм.]')
+      plt.ylabel(f'{plot_col[n]}')
       plt.plot(self.input_indices, inputs[n, :, plot_col_index],
               label='Вхідні дані', marker='.', zorder=-10)
 
@@ -224,48 +224,58 @@ class WindowGenerator():
       self._example = result
     return result
 
-w2 = WindowGenerator(85, 20, 20)
-# об'єднання трьох зрізів (вибірок) для передачі у вікно даних
-#  example_window = tf.stack([np.array(train_df[:w2.total_window_size]),
-#                            np.array(train_df[100:100+w2.total_window_size]),
-#                            np.array(train_df[200:200+w2.total_window_size])])
-
-example_window = tf.stack([np.array(train_df[:w2.total_window_size]),
-                           np.array(train_df[200:200+w2.total_window_size]),
-                           np.array(train_df[400:400+w2.total_window_size])])
-
-example_inputs, example_labels = w2.split_window(example_window)
-
-print('Розміри подані як: (batch, time, features)')
-print(f'Розміри вікна: {example_window.shape}')
-print(f'Розміри вхідних даних: {example_inputs.shape}')
-print(f'Розміри міток для перевірки: {example_labels.shape}')
-
-
 # прогнозування декількох кроків
-OUT_STEPS = 4    
-multi_window = WindowGenerator(input_width=8,            # 24 вхідних кроки
-                               label_width=OUT_STEPS,     # 24 позначки (= кількості прогнозованих для порівняння правильності прогнозу)
-                               shift=OUT_STEPS)           # 24 прогнозованих кроки
-for i in range(len(plot_cols)):
-    multi_window.plot(plot_col=plot_cols[i])   # побудова графіку вікна даних
-    plt.show()
+multi_window = WindowGenerator(
+                                input_width=TRAIN_COUNT,            # вхідних кроки
+                                label_width=OUT_STEPS,     # позначки (= кількості прогнозованих для порівняння правильності прогнозу)
+                                shift=OUT_STEPS)           # прогнозованих кроки
+# multi_inputs, multi_labels = multi_window.split_window(multi_window)
+
+# print('Розміри подані як: (batch, time, features)')
+# print(f'Розміри вікна: {multi_window.shape}')
+# print(f'Розміри вхідних даних: {multi_window.input_width.shape}')
+# print(f'Розміри міток для перевірки: {multi_window.label_indices.shape}')
+
+# for i in range(len(plot_cols)):
+multi_window.plot(plot_col=plot_cols)   # побудова графіку вікна даних
+plt.show()
 
 # налаштування згорткової мережі для прогнозу
 CONV_WIDTH = 3  # ширина згортки
 multi_conv_model = tf.keras.Sequential([
-    # Обгортка , що подає лямбда-вираз у вигляді шару [batch, time, features] => [batch, CONV_WIDTH, features]
-    tf.keras.layers.Lambda(lambda x: x[:, -CONV_WIDTH:, :]),
-    # Шар виконання 1-вимірної згортки => [batch, 1, conv_units]
-    tf.keras.layers.Conv1D(256, activation='relu', kernel_size=(CONV_WIDTH)),
-    # Шар ущільнення => [batch, 1,  out_steps*features]
-    tf.keras.layers.Dense(OUT_STEPS*num_features,
-                          kernel_initializer=tf.initializers.zeros()),
-    # Шар перетворення даних до заданого виду => [batch, out_steps, features]
-    tf.keras.layers.Reshape([OUT_STEPS, num_features])
+
+    tf.keras.layers.Lambda(lambda x: x[:, -CONV_WIDTH:, :]),     # Обгортка , що подає лямбда-вираз у вигляді шару [batch, time, features] => [batch, CONV_WIDTH, features]
+    tf.keras.layers.Conv1D(256, activation='relu', kernel_size=(CONV_WIDTH)),   # Шар виконання 1-вимірної згортки => [batch, 1, conv_units] 
+    tf.keras.layers.Dense(OUT_STEPS*num_features, kernel_initializer=tf.initializers.zeros()), # Шар ущільнення => [batch, 1,  out_steps*features]
+    tf.keras.layers.Reshape([OUT_STEPS, num_features])  # Шар перетворення даних до заданого виду => [batch, out_steps, features]
 ])
 
-history = compile_and_fit(multi_conv_model, multi_window) # компіляція моделі та її тренування на основі даних з [multi_window]
+model = tf.keras.Sequential([
+    tf.keras.layers.LSTM(200, activation='relu', input_shape=(TRAIN_COUNT, OUT_STEPS)),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(OUT_STEPS)
+])
+
+history = compile_and_fit(model, multi_window) # компіляція моделі та її тренування на основі даних з [multi_window]
+
+
+def plot_train_history(history, title):
+  loss = history.history['loss']
+  # val_loss = history.history['val_loss']
+
+  epochs = range(len(loss))
+
+  plt.figure()
+
+  plt.plot(epochs, loss, 'b', label='Training loss')
+  # if(len(history.history['val_loss'])): 
+  #   plt.plot(epochs, history.history['val_loss'], 'r', label='Validation loss')
+  plt.title(title)
+  plt.legend()
+
+  plt.show()
+
+plot_train_history(history, 'Training and validation loss')
 
 # очистка вікна виводу та створення змінних для аналізу продуктивності
 IPython.display.clear_output()
@@ -273,9 +283,7 @@ multi_val_performance = {}
 multi_performance = {}
 
 # multi_val_performance['Conv'] = multi_conv_model.evaluate(multi_window.val)             # продуктивність відповідно до значень
-multi_performance['Conv'] = multi_conv_model.evaluate(multi_window.test, verbose=0)     # продуктивність моделі
-for i in range(len(plot_cols)):
-    multi_window.plot(multi_conv_model, plot_cols[i])                                                     # побудова графіка отриманих результатів
-    # timer.start()
-    plt.show()
+# multi_performance['Conv'] = multi_conv_model.evaluate(multi_window.test)                  # продуктивність моделі
+multi_window.plot(multi_conv_model, plot_cols)                                                     # побудова графіка отриманих результатів
+plt.show()
 
